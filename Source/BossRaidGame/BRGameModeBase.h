6358b088a19b90ffc54ff9931615ccb51fac4362
No1playblock <jghan2000@naver.com>
2025-05-29 16:09:20 +0900

    Add: "BRGameModeBase 생성"

---
+++ Source/BossRaidGame/BRGameModeBase.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BRGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOSSRAIDGAME_API ABRGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	ABRGameModeBase();
+};
