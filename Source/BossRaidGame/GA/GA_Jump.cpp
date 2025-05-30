edc8a1ab89becfbff7e5e1a5c62376483e72e94c
No1playblock <jghan2000@naver.com>
2025-05-31 05:04:39 +0900

    Add: "Add GA_Jump"

---
+++ Source/BossRaidGame/GA/GA_Jump.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GA/GA_Jump.h"
+#include "GameFramework/Character.h"
+#include "AT/AT_JumpAndWaitForLanding.h"
+UGA_Jump::UGA_Jump()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+
+}
+
+bool UGA_Jump::CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags, OUT FGameplayTagContainer* OptionalRelevantTags) const
+{
+	bool bResult = Super::CanActivateAbility(Handle, ActorInfo, SourceTags, TargetTags, OptionalRelevantTags);
+	if (!bResult)
+	{
+		return false;
+	}
+
+	const ACharacter* Character = Cast<ACharacter>(ActorInfo->AvatarActor.Get());
+	return (Character && Character->CanJump());
+}
+
+void UGA_Jump::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	UAT_JumpAndWaitForLanding* JumpAndWaitingForLandingTask = UAT_JumpAndWaitForLanding::CreateTask(this);
+	JumpAndWaitingForLandingTask->OnComplete.AddDynamic(this, &UGA_Jump::OnLandedCallback);
+	JumpAndWaitingForLandingTask->ReadyForActivation();
+
+}
+
+void UGA_Jump::InputReleased(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo)
+{
+
+	ACharacter* Character = Cast<ACharacter>(ActorInfo->AvatarActor.Get());
+
+	Character->StopJumping();
+
+}
+
+void UGA_Jump::OnLandedCallback()
+{
+	bool bReplicatedEndAbility = true;
+	bool bWasCancelled = false;
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, bReplicatedEndAbility, bWasCancelled);
+}
