d1cc4a3f93b5a44071aa1ed846f4986d963864ac
No1playblock <jghan2000@naver.com>
2025-06-03 01:31:43 +0900

    Mod: "Start, End Location 추가, GameEffect 적용"
    Start와 End를 추가해서 Sweep 범위늘려 정확도를 높임.
    GameEffect를 사용해서 대미지 적용

--- Source/BossRaidGame/GA/AT/AT_CollisionSweep.cpp
+++ Source/BossRaidGame/GA/AT/AT_CollisionSweep.cpp
@@ -3,7 +3,10 @@
 
 #include "GA/AT/AT_CollisionSweep.h"
 #include "Character/GASCharacterPlayer.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
 #include "AT_CollisionSweep.h"
+#include "GA/GA_AttackHitCheck.h"
 
 
 UAT_CollisionSweep::UAT_CollisionSweep()
@@ -22,7 +25,7 @@ void UAT_CollisionSweep::Activate()
 	Super::Activate();
 	PerformSweep();
 
-	GetWorld()->GetTimerManager().SetTimer(SweepTimerHandle, this, &UAT_CollisionSweep::PerformSweep, 0.001f, true); // 30fps 기준 반복
+	GetWorld()->GetTimerManager().SetTimer(SweepTimerHandle, this, &UAT_CollisionSweep::PerformSweep, 0.011f, true); // 30fps 기준 반복
 }
 
 void UAT_CollisionSweep::OnDestroy(bool AbilityEnded)
@@ -42,12 +45,21 @@ void UAT_CollisionSweep::PerformSweep()
 	USkeletalMeshComponent* Mesh = Character->GetMesh();
 	if (!Mesh) return;
 	const FName SocketName = TEXT("root_weaponSocket"); // 사용할 Socket 이름
-
+	const FName SocketEndName = TEXT("root_weaponEndSocket"); // 사용할 Socket 이름
+	FVector EndLocation = Mesh->GetSocketLocation(SocketEndName);
+	
 	FVector Location = Mesh->GetSocketLocation(SocketName);
 	FQuat Rotation = Mesh->GetSocketQuaternion(SocketName);
-	FTransform SocketLocalTransform = Mesh->GetSocketTransform(SocketName, ERelativeTransformSpace::RTS_Actor);
+
 	FVector BoxExtent = FVector(6.f, 105.f, 13.f); // 원하는 판정 범위
 
+	if (!bHasLastLocation)
+	{
+		LastLocation = Location;
+		EndLastLocation = EndLocation;
+		bHasLastLocation = true;
+		return;
+	}
 
 	FCollisionShape BoxShape = FCollisionShape::MakeBox(BoxExtent);
 	TArray<FHitResult> Hits;
@@ -57,8 +69,8 @@ void UAT_CollisionSweep::PerformSweep()
 
 	bool bHit = GetWorld()->SweepMultiByChannel(
 		Hits,
-		Location,
-		Location, // Static sweep (이동 없음)
+		LastLocation,
+		Location, 
 		Rotation,
 		ECollisionChannel::ECC_Pawn, // 설정한 채널 사용
 		BoxShape,
@@ -69,18 +81,43 @@ void UAT_CollisionSweep::PerformSweep()
 
 
 	FColor DrawColor = bHit ? FColor::Green : FColor::Red;
-	DrawDebugBox(GetWorld(), Location, BoxExtent, Rotation, DrawColor, false, 2.0f);
-
+	//DrawDebugBox(GetWorld(), Location, BoxExtent, Rotation, DrawColor, false, 2.0f);
+	DrawDebugLine(GetWorld(), EndLastLocation, EndLocation, DrawColor, false, 1.0f, 0, 1.f);
 
 #endif
+	LastLocation = Location;
+	EndLastLocation = EndLocation;
 
+	TSubclassOf<UGameplayEffect> DamageEffectClass = nullptr;
+	if (const auto* GA = Cast<UGA_AttackHitCheck>(Ability))
+	{
+		DamageEffectClass = GA->GetDamageEffectClass();
+	}
+
+	if (!DamageEffectClass) return;
 
 	for (const FHitResult& Hit : Hits)
 	{
-		if (AActor* HitActor = Hit.GetActor())
+		AActor* HitActor = Hit.GetActor();
+		if (!HitActor || HitActor == GetAvatarActor()) continue;
+
+		if (HitActors.Contains(HitActor)) continue;
+
+		HitActors.Add(HitActor); // 최초 1회만 통과
+
+		UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(HitActor);
+		if (!TargetASC) continue;
+
+		UAbilitySystemComponent* SourceASC = Ability->GetAbilitySystemComponentFromActorInfo();
+		if (!SourceASC) return;
+
+		FGameplayEffectContextHandle EffectContext = SourceASC->MakeEffectContext();
+		FGameplayEffectSpecHandle SpecHandle = SourceASC->MakeOutgoingSpec(DamageEffectClass, 1.f, EffectContext);
+
+		if (SpecHandle.IsValid())
 		{
-			// 충돌 처리 로직
-			UE_LOG(LogTemp, Log, TEXT("Hit actor: %s"), *HitActor->GetName());
+			TargetASC->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
 		}
 	}
+	
 }
