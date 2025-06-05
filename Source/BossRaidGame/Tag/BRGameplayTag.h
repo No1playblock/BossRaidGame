e081fd882053fe0fafc48582792c69ed9b21cd02
No1playblock <jghan2000@naver.com>
2025-06-05 22:16:36 +0900

    Add: "BRGameplayTag.h 생성"

---
+++ Source/BossRaidGame/Tag/BRGameplayTag.h
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameplayTagContainer.h"
+
+//#define BRTAG_DATA_DAMAGE FGameplayTag::RequestGameplayTag(FName("Data.Damage"))
+#define BRTAG_CHARACTER_ISDEAD FGameplayTag::RequestGameplayTag(FName("Character.State.IsDead"))
