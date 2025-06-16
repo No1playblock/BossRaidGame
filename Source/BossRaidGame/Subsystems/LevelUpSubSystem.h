f50c61dcdadf6f3bcd23f1b60115fc9defbe25d4
No1playblock <jghan2000@naver.com>
2025-06-17 00:50:56 +0900

    Create LevelUpSubSystem.h

---
+++ Source/BossRaidGame/Subsystems/LevelUpSubSystem.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/LocalPlayerSubsystem.h"
+#include "GameData/StatChoiceData.h" // FStatChoiceInfo
+#include "LevelUpSubsystem.generated.h"
+
+class UDataTable;
+class UUserWidget;
+class ULevelUpScreenWidget;
+class AGASCharacterPlayer;
+
+UCLASS(Config = Game)
+class BOSSRAIDGAME_API ULevelUpSubsystem : public ULocalPlayerSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+
+	void ShowLevelUpChoices(AGASCharacterPlayer* PlayerCharacter, int32 NewLevel);
+	void ApplyStatChoice(const FStatChoiceInfo& ChoiceInfo);
+
+protected:
+	UPROPERTY()
+	TObjectPtr<UDataTable> LevelStatDataTable;
+
+	UPROPERTY()
+	TSubclassOf<UUserWidget> StatChoiceWidgetClass;
+
+	UPROPERTY(Config)
+	TSoftObjectPtr<UDataTable> LevelStatDataTablePath;
+
+	UPROPERTY(Config)
+	TSoftClassPtr<UUserWidget> StatChoiceWidgetClassPath;
+
+private:
+	void OnDataTableLoaded();
+	void OnWidgetClassLoaded();
+
+	UPROPERTY()
+	TObjectPtr<UUserWidget> CurrentStatChoiceWidget;
+
+	UPROPERTY()
+	TWeakObjectPtr<AGASCharacterPlayer> TargetPlayer;
+};
