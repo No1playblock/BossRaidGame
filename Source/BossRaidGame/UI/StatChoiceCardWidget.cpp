2339a77f514e3abc53e8a28c7100d7462bae2a2b
No1playblock <jghan2000@naver.com>
2025-06-17 00:51:37 +0900

    create: Add StatChoiceCardWidget

---
+++ Source/BossRaidGame/UI/StatChoiceCardWidget.cpp
@@ -0,0 +1,32 @@
+#include "UI/StatChoiceCardWidget.h"
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
+
+void UStatChoiceCardWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (SelectButton)
+	{
+		SelectButton->OnClicked.AddDynamic(this, &UStatChoiceCardWidget::OnSelectButtonClicked);
+	}
+}
+
+void UStatChoiceCardWidget::SetChoiceInfo(const FStatChoiceInfo& InChoiceInfo)
+{
+	CurrentChoiceInfo = InChoiceInfo;
+
+	if (StatNameText)
+	{
+		StatNameText->SetText(CurrentChoiceInfo.DisplayName);
+	}
+	if (StatValueText)
+	{
+		StatValueText->SetText(CurrentChoiceInfo.DisplayValue);
+	}
+}
+
+void UStatChoiceCardWidget::OnSelectButtonClicked()
+{
+	OnChoiceCardSelected.Broadcast(CurrentChoiceInfo);
+}
---
+++ Source/BossRaidGame/UI/StatChoiceCardWidget.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "GameData/StatChoiceData.h" // FStatChoiceInfo
+#include "StatChoiceCardWidget.generated.h"
+
+// 블루프린트에서 바인딩할 수 있는 다이나믹 멀티캐스트 델리게이트 선언
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnChoiceCardSelectedSignature, const FStatChoiceInfo&, ChoiceInfo);
+
+UCLASS()
+class BOSSRAIDGAME_API UStatChoiceCardWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	void SetChoiceInfo(const FStatChoiceInfo& InChoiceInfo);
+
+	UPROPERTY(BlueprintAssignable, Category = "ChoiceCard")
+	FOnChoiceCardSelectedSignature OnChoiceCardSelected;
+
+protected:
+	virtual void NativeConstruct() override;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<class UButton> SelectButton;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<class UTextBlock> StatNameText;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<class UTextBlock> StatValueText;
+
+private:
+	UFUNCTION()
+	void OnSelectButtonClicked();
+
+private:
+	UPROPERTY()
+	FStatChoiceInfo CurrentChoiceInfo;
+};
