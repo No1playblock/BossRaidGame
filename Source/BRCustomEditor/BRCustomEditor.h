ab154fa8d669aff809697a837d9c552274f6098e
No1playblock <jghan2000@naver.com>
2025-06-19 01:22:20 +0900

    feat: Add the SkillTreeTab

---
+++ Source/BRCustomEditor/BRCustomEditor.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Modules/ModuleInterface.h"
+
+class FBRCustomEditorModule : public IModuleInterface
+{
+public:
+    virtual void StartupModule() override;
+    
+    virtual void ShutdownModule() override;
+
+   
+private:
+
+    void RegisterMenus();
+
+    void HandleMenuClicked();
+    TSharedRef<SDockTab> OnSpawnPluginTab(const FSpawnTabArgs& Args);
+
+    TSharedRef<SDockTab> OnSpawnPluginTab_SkillTree(const FSpawnTabArgs& Args);
+
+    // [추가] 스킬 트리 에디터 메뉴 클릭 핸들러
+    void HandleMenuClicked_SkillTree();
+    
+};
