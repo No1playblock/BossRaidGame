c6cfdbed09f8bb5ccd59998f7400b9130db1d7cd
No1playblock <jghan2000@naver.com>
2025-06-19 01:27:04 +0900

    feat: Add SkillTreeEdGraphSchema

---
+++ Source/BRCustomEditor/Public/SkillTreeEdGraphSchema.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EdGraph/EdGraphSchema.h"
+#include "SkillTreeEdGraphSchema.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BRCUSTOMEDITOR_API USkillTreeEdGraphSchema : public UEdGraphSchema
+{
+	GENERATED_BODY()
+	
+};
