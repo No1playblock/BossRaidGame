e9ea870ffcc236c1af7c10bb0d743515c14df4f8
No1playblock <jghan2000@naver.com>
2025-06-19 09:26:57 +0900

    feat: Add publicDependencyModule GraphEditor, BlueprintGraph

---
+++ Source/BRCustomEditor/BRCustomEditor.Build.cs
@@ -0,0 +1,21 @@
+using UnrealBuildTool;
+
+public class BRCustomEditor : ModuleRules
+{
+    public BRCustomEditor(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Slate", "SlateCore",
+                                                                "EditorStyle", "UnrealEd", "PropertyEditor", "Projects", 
+                                                                    "LevelEditor", "ToolMenus", "BossRaidGame", "GraphEditor", "BlueprintGraph"
+        });
+
+        PrivateDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks" });
+
+
+        PrivateIncludePaths.AddRange(new string[] {
+            "BRCustomEditor/Public"
+        }); 
+    }
+}
