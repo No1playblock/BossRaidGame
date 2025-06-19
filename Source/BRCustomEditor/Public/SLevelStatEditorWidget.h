9d00f30108130cbaf128275a6d1469b8d5c6e16c
No1playblock <jghan2000@naver.com>
2025-06-19 09:28:16 +0900

    refactor: Change the path

---
+++ Source/BRCustomEditor/Public/SLevelStatEditorWidget.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "Widgets/SCompoundWidget.h"
+#include "Widgets/Views/SListView.h"
+#include "GameData/LevelStatData.h"
+
+// 전방 선언
+class UDataTable;
+
+class SLevelStatEditorWidget : public SCompoundWidget
+{
+public:
+	SLATE_BEGIN_ARGS(SLevelStatEditorWidget) {}
+	SLATE_END_ARGS()
+
+	void Construct(const FArguments& InArgs);
+
+private:
+	TWeakObjectPtr<UDataTable> TargetDataTable;
+
+	TArray<TSharedPtr<FLevelStatRow>> StatRows;
+
+	TSharedPtr<SListView<TSharedPtr<FLevelStatRow>>> StatListView;
+
+	TSharedRef<ITableRow> OnGenerateRow(TSharedPtr<FLevelStatRow> InItem, const TSharedRef<STableViewBase>& OwnerTable);
+
+	FString SelectedDataTablePath;
+private:
+	FORCEINLINE FString GetSelectedDataTablePath() const { return SelectedDataTablePath; };
+
+	FReply OnClick_AddRow();
+
+	FReply OnClick_Save();
+
+	void OnDataTableChanged(const FAssetData& AssetData);
+
+	FReply OnClick_DeleteRow(TSharedPtr<FLevelStatRow> ItemToDelete); // <<< [추가]
+
+
+};
