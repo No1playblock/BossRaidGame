d10876bd8981e96a0ea4733d4076940280493e3b
No1playblock <jghan2000@naver.com>
2025-06-20 19:16:29 +0900

    feat: Add the Binding and Getter Function

--- Source/BRCustomEditor/Public/SSkillTreeGraphNode.h
+++ Source/BRCustomEditor/Public/SSkillTreeGraphNode.h
@@ -16,5 +16,19 @@ public:
 
 private:
     // [추가] 노드 제목을 가져오기 위한 헬퍼 함수
-    FText GetTitle() const;
+	FText GetTitle() const;
+	FText GetDescription() const;
+	const FSlateBrush* GetIconBrush() const;
+	TOptional<int32> GetSkillPointCost() const;
+	TOptional<float> GetSkillDamage() const;
+
+	// --- Commit Handlers (UI에서 값을 수정한 뒤 호출될 함수들) ---
+	void OnTitleCommitted(const FText& InText, ETextCommit::Type CommitInfo);
+	void OnDescriptionCommitted(const FText& InText, ETextCommit::Type CommitInfo);
+	void OnSkillPointCostCommitted(int32 InValue, ETextCommit::Type CommitInfo);
+	void OnSkillDamageCommitted(float InValue, ETextCommit::Type CommitInfo);
+
+	// 아이콘을 표시하기 위한 Slate 브러시
+	TSharedPtr<FSlateBrush> IconBrush;
+
 };
