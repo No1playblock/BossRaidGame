b9f1f3f04d592aa3dcc757feb8c57755a47e33bd
No1playblock <jghan2000@naver.com>
2025-06-28 01:36:35 +0900

    feat: Add BRPlayerController

---
+++ Source/BossRaidGame/Player/BRPlayerController.cpp
@@ -0,0 +1,70 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/BRPlayerController.h"
+#include "Blueprint/UserWidget.h" // CreateWidget 사용
+#include "UI/SkillTreeWidget.h"      // SkillTreeWidget 참조 (수정한 부분입니다)
+#include "Kismet/GameplayStatics.h" 
+ABRPlayerController::ABRPlayerController()
+{
+	static ConstructorHelpers::FClassFinder<USkillTreeWidget> SkillTreeWidgetBPClass(TEXT("/Game/Blueprints/UI/WBP_SkillTree.WBP_SkillTree_C"));
+	if (SkillTreeWidgetBPClass.Succeeded())
+	{
+		SkillTreeWidgetClass = SkillTreeWidgetBPClass.Class; // 수정한 부분입니다.
+	}
+
+	static ConstructorHelpers::FObjectFinder<UInputAction> ToggleSkillTreeAction(TEXT("/Script/EnhancedInput.InputAction'/Game/Blueprints/Input/InputAction/IA_ToggleSkillUI.IA_ToggleSkillUI'"));
+	if (ToggleSkillTreeAction.Succeeded())
+	{
+		IA_ToggleSkillTree = ToggleSkillTreeAction.Object; // 수정한 부분입니다.
+	}
+
+	static ConstructorHelpers::FObjectFinder<UInputMappingContext> MappingContext(TEXT("/Script/EnhancedInput.InputMappingContext'/Game/Blueprints/Input/IMC_Default.IMC_Default'"));
+	if (MappingContext.Succeeded())
+	{
+		DefaultMappingContext = MappingContext.Object; // 수정한 부분입니다.
+	}
+}
+
+void ABRPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 수정한 부분입니다. EnhancedInputSubsystem에 Mapping 추가
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		Subsystem->AddMappingContext(DefaultMappingContext, 0);
+	}
+}
+
+void ABRPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	if (UEnhancedInputComponent* EnhancedInput = Cast<UEnhancedInputComponent>(InputComponent))
+	{
+		EnhancedInput->BindAction(IA_ToggleSkillTree, ETriggerEvent::Triggered, this, &ABRPlayerController::ToggleSkillTreeUI);
+	}
+}
+
+void ABRPlayerController::ToggleSkillTreeUI()
+{
+	if (!SkillTreeWidgetInstance && SkillTreeWidgetClass)
+	{
+		SkillTreeWidgetInstance = CreateWidget<USkillTreeWidget>(this, SkillTreeWidgetClass);
+	}
+	if (SkillTreeWidgetInstance)
+	{
+		const bool bVisible = SkillTreeWidgetInstance->IsInViewport();
+		if (!bVisible)
+		{
+			SkillTreeWidgetInstance->AddToViewport();
+			SetShowMouseCursor(true);
+			// 탭키만 UI 입력에 포함
+			SetInputMode(FInputModeUIOnly());
+			SkillTreeWidgetInstance->SetKeyboardFocus();
+			UGameplayStatics::SetGamePaused(GetWorld(), true);
+
+		}
+	}
+}
---
+++ Source/BossRaidGame/Player/BRPlayerController.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "InputAction.h"
+#include "InputMappingContext.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "BRPlayerController.generated.h"
+
+/**
+ * 
+ */
+class USkillTreeWidget;
+
+UCLASS()
+class BOSSRAIDGAME_API ABRPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	ABRPlayerController();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+	void ToggleSkillTreeUI(); // UI를 여닫는 함수
+
+protected:
+	// 수정한 부분입니다.
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	TObjectPtr<UInputMappingContext> DefaultMappingContext;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	TObjectPtr<UInputAction> IA_ToggleSkillTree;
+
+	
+
+	UPROPERTY()
+	USkillTreeWidget* SkillTreeWidgetInstance;
+
+	UPROPERTY(EditAnywhere, Category = "UI")
+	TSubclassOf<USkillTreeWidget> SkillTreeWidgetClass;
+
+};
