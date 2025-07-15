b9b3c8347b65e37525a9f3cfdba93d3f36e4a8b3
No1playblock <jghan2000@naver.com>
2025-07-15 09:55:35 +0900

    feat: AI 캐릭터의 임의 위치 이동 기능 추가
    `BTTask_SetRandomLocation` 클래스에 AI 캐릭터가 탐색 가능한 임의의 위치로 이동하도록 설정하는 기능을 추가하였다.
    
    AI 컨트롤러와 NPC 캐릭터를 가져오고, 내비게이션 시스템을 통해 임의의 위치를 찾는 로직을 구현하였다.
    
    찾은 위치는 디버그 포인트로 시각화되며, 블랙보드에 저장된다.
    
    `Radius` 변수를 추가하여 탐색 가능한 반경을 설정할 수 있도록 하였다.

---
+++ Source/BossRaidGame/AI/Task/BTTask_SetRandomLocation.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/Task/BTTask_SetRandomLocation.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AI/BRAIController.h"
+#include "Character/NonPlayerGASCharacter.h"
+#include "NavigationSystem.h"
+#include "Blueprint/AIBlueprintHelperLibrary.h"
+
+EBTNodeResult::Type UBTTask_SetRandomLocation::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	ABRAIController* ThisAIController = Cast<ABRAIController>(OwnerComp.GetAIOwner());
+	if (!ThisAIController) 	return EBTNodeResult::Failed;
+
+	ANonPlayerGASCharacter* ThisMobCharacter = Cast<ANonPlayerGASCharacter>(ThisAIController->GetPawn());
+	if (!ThisMobCharacter) 	return EBTNodeResult::Failed;
+
+	UNavigationSystemV1* Navsystem = UNavigationSystemV1::GetNavigationSystem(GetWorld());
+	if (Navsystem == nullptr) return EBTNodeResult::Failed;
+
+	FNavLocation RandomNavLocation;
+	bool isRandomPoint = Navsystem->GetRandomPointInNavigableRadius(ThisMobCharacter->GetActorLocation(), Radius, RandomNavLocation, NULL);
+
+	if (isRandomPoint)
+	{
+		DrawDebugPoint(
+			GetWorld(),                 // 현재 월드
+			RandomNavLocation.Location, // 점을 그릴 위치 (FNavLocation에서 FVector를 가져옴)
+			20.0f,                      // 점의 크기 (반지름)
+			FColor::Green,              // 점의 색상
+			false,                      // 한 프레임만 보일지 여부 (false = 지속)
+			5.0f                        // 몇 초 동안 보일지 (지속 시간)
+		);
+		OwnerComp.GetBlackboardComponent()->SetValueAsVector(TEXT("RandomLocation"), RandomNavLocation.Location);
+		/*UAIBlueprintHelperLibrary::SimpleMoveToLocation(ThisAIController, RandomNavLocation.Location);
+		UE_LOG(LogTemp, Warning, TEXT("Moving to random location: %s"), *RandomNavLocation.Location.ToString());*/
+		return EBTNodeResult::Succeeded;
+	}
+	UE_LOG(LogTemp, Warning, TEXT("Failed to find a random navigable point within radius: %f"), Radius);
+	return EBTNodeResult::Failed;
+}
