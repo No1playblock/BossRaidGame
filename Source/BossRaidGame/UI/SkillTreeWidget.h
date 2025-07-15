d6b1159ee1031139ca2387f46e8ef3d7bddb2cb6
No1playblock <jghan2000@naver.com>
2025-07-15 10:39:09 +0900

    feat: 생명주기 관리 기능 추가
    `SkillTreeWidget.h`에 `NativeDestruct` 가상 함수를 추가하여 클래스의 생명주기 관리를 개선함.

--- Source/BossRaidGame/UI/SkillTreeWidget.h
+++ Source/BossRaidGame/UI/SkillTreeWidget.h
@@ -34,6 +34,8 @@ protected:
 	
 	virtual void NativeConstruct() override;
 
+	virtual void NativeDestruct() override;	
+
 	virtual FReply NativeOnKeyDown(const FGeometry& InGeometry, const FKeyEvent& InKeyEvent) override;
 
 	UFUNCTION()
