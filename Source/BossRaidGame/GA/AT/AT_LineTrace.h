62bdcfb11cb84d67d65d638281eea2e7308ec1f2
No1playblock <jghan2000@naver.com>
2025-07-15 09:58:51 +0900

    refactor: AT_LineTrace.h에서 의존성 제거
    AT_LineTrace.h 파일에서 불필요한 헤더 파일인
    AT_ShootBase.h의 포함을 삭제하여 의존성을 줄임.

--- Source/BossRaidGame/GA/AT/AT_LineTrace.h
+++ Source/BossRaidGame/GA/AT/AT_LineTrace.h
@@ -4,7 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "Abilities/Tasks/AbilityTask.h"
-#include "AT_ShootBase.h"
 #include "AT_LineTrace.generated.h"
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnLineTraceHit, const FHitResult&, HitResult);
