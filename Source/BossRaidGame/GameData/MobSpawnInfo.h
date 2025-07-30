4e1eada52e7c82ee4123c55efbccb2a60a285665
No1playblock <jghan2000@naver.com>
2025-07-30 15:08:27 +0900

    feat: 몬스터 스폰 정보 구조체 추가
    `MobSpawnInfo.h` 파일에 `FMobSpawnInfo` 구조체를 추가하여 몬스터의 클래스, 스폰 수, 이펙트, 사운드, 건강, 속도, 경험치 등의 속성을 정의함.

---
+++ Source/BossRaidGame/GameData/MobSpawnInfo.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "MobSpawnInfo.generated.h"
+
+USTRUCT(BlueprintType)
+struct FMobSpawnInfo : public FTableRowBase
+{
+    GENERATED_BODY()
+
+public:
+    // 스폰될 몬스터 블루프린트
+    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    TSubclassOf<ACharacter> MonsterClass;
+
+    // 스폰할 몬스터 수
+    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    int32 SpawnCount;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    UParticleSystem* SpawnVFX;
+
+    // 스폰 시 사용할 사운드 (SFX)
+    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    USoundBase* SpawnSFX;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Stats")
+    float Health;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Stats")
+    float WalkSpeed;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Stats")
+    float RunSpeed;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Stats")
+    float Experience;
+};
