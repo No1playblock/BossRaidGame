b38e639da91f5dd736f9e47775307bc603b169be
No1playblock <jghan2000@naver.com>
2025-07-30 15:12:01 +0900

    feat: 캐릭터 상태 및 속도 태그 추가
    게임 내에서 캐릭터의 상태를 나타내는
    `BRTAG_CHARACTER_ISCHASING` 태그와
    속도를 나타내는 `BRTAG_DATA_SPEED` 태그를 추가하여
    가독성을 높이고 기능성을 향상시킴.

--- Source/BossRaidGame/Tag/BRGameplayTag.h
+++ Source/BossRaidGame/Tag/BRGameplayTag.h
@@ -7,3 +7,5 @@
 #define BRTAG_DATA_DAMAGE_QSKILL FGameplayTag::RequestGameplayTag(FName("Data.Damage.QSkill"))
 #define BRTAG_CHARACTER_ISDEAD FGameplayTag::RequestGameplayTag(FName("Character.State.IsDead"))
 #define BRTAG_DATA_EXPERIENCE FGameplayTag::RequestGameplayTag(FName("Data.Experience"))
+#define BRTAG_CHARACTER_ISCHASING FGameplayTag::RequestGameplayTag(FName("Character.State.IsChasing"))
+#define BRTAG_DATA_SPEED FGameplayTag::RequestGameplayTag(FName("Data.Speed"))
