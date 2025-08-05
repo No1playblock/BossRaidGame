5e9acc8963ea45c773db0fff314efaf18c584977
No1playblock <jghan2000@naver.com>
2025-08-05 15:46:59 +0900

    feat: 플레이어를 따라가는 HitLocation AT 추가
    UAT_MultiHitAtLocation 클래스를 구현하여
    여러 번의 히트를 처리하는 기능을 추가함.
    
    MultiHitAtLocation static 함수를 통해
    능력 관련 매개변수를 받아 새로운 작업 인스턴스를 생성함.
    
    Activate 메서드에서 AI 컨트롤러의
    블랙보드 컴포넌트를 가져오는 로직을 추가함.
    
    PerformNextHit 메서드에서
    타겟 위치를 가져오고 오버랩 결과를 처리함.
    
    타이머를 사용하여 히트를 지연시키고,
    히트 완료 시 OnFinished 델리게이트를 호출함.
    
    OnDestroy 메서드에서 작업 종료 시
    타이머를 정리하는 기능을 포함함.

---
+++ Source/BossRaidGame/GA/AT/AT_MultiHitAtLocation.cpp
@@ -0,0 +1,119 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GA/AT/AT_MultiHitAtLocation.h"
+#include "AbilitySystemComponent.h"
+#include "GameFramework/Actor.h"
+#include "TimerManager.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine/OverlapResult.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+
+UAT_MultiHitAtLocation* UAT_MultiHitAtLocation::MultiHitAtLocation(UGameplayAbility* OwningAbility, FName TargetLocationKeyName, int32 InNumberOfHits, float InDelayBetweenHits, float InAreaRadius, TEnumAsByte<ECollisionChannel> InTargetChannel, UParticleSystem* InAreaVFX, USoundBase* InAreaSFX)
+{
+	UAT_MultiHitAtLocation* MyTask = NewAbilityTask<UAT_MultiHitAtLocation>(OwningAbility);
+	MyTask->TargetLocationKeyName = TargetLocationKeyName;
+	MyTask->NumberOfHits = InNumberOfHits;
+	MyTask->DelayBetweenHits = InDelayBetweenHits;
+	MyTask->AreaRadius = InAreaRadius;
+	MyTask->TargetChannel = InTargetChannel;
+	MyTask->AreaVFX = InAreaVFX;
+	MyTask->AreaSFX = InAreaSFX;
+	return MyTask;
+}
+
+void UAT_MultiHitAtLocation::Activate()
+{
+	Super::Activate();
+	
+	APawn* AvatarPawn = Cast<APawn>(GetAvatarActor());
+	if (AvatarPawn)
+	{
+		// 2. 폰을 소유하고 있는 컨트롤러를 가져옵니다.
+		AController* Controller = AvatarPawn->GetController();
+
+		// 3. AI 컨트롤러로 캐스팅하고 블랙보드를 가져와 저장합니다.
+		if (AAIController* AIController = Cast<AAIController>(Controller))
+		{
+			BlackboardComp = AIController->GetBlackboardComponent();
+		}
+	}
+
+	CurrentHitCount = 0;
+	PerformNextHit();
+}
+
+
+void UAT_MultiHitAtLocation::PerformNextHit()
+{
+	if (CurrentHitCount >= NumberOfHits)
+	{
+		if (OnFinished.IsBound())
+		{
+			OnFinished.Broadcast();
+		}
+		EndTask();
+		return;
+	}
+
+	if (!BlackboardComp.IsValid())
+	{
+		EndTask();
+		return;
+	}
+
+	const FVector TargetLocation = BlackboardComp->GetValueAsVector(TargetLocationKeyName);
+	if (TargetLocation == FVector::ZeroVector)
+	{
+		EndTask();
+		return;
+	}
+
+	AActor* OwnerActor = GetAvatarActor();
+	UWorld* World = GetWorld();
+	if (!World || !OwnerActor)
+	{
+		EndTask();
+		return;
+	}
+
+	if (AreaVFX)
+	{
+		FVector VFXLocation = TargetLocation;
+		VFXLocation.Z -= 90.0f; 
+		UGameplayStatics::SpawnEmitterAtLocation(World, AreaVFX, VFXLocation);
+	}
+	if (AreaSFX)
+	{
+		UGameplayStatics::PlaySoundAtLocation(World, AreaSFX, TargetLocation);
+	}
+
+	TArray<FOverlapResult> OverlapResults;
+	FCollisionShape SphereShape = FCollisionShape::MakeSphere(AreaRadius);
+	FCollisionQueryParams QueryParams;
+	QueryParams.AddIgnoredActor(OwnerActor);
+
+	World->OverlapMultiByChannel(OverlapResults, TargetLocation, FQuat::Identity, TargetChannel, SphereShape, QueryParams);
+
+	const FColor DebugColor = OverlapResults.Num() > 0 ? FColor::Green : FColor::Red;
+	float DebugLifetime = DelayBetweenHits > 0.f ? DelayBetweenHits : 2.0f;
+	DrawDebugSphere(World, TargetLocation, AreaRadius, 24, DebugColor, false, DebugLifetime);
+
+	if (OverlapResults.Num() > 0 && OnHit.IsBound())
+	{
+		OnHit.Broadcast(OverlapResults);
+	}
+
+	CurrentHitCount++;
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &UAT_MultiHitAtLocation::PerformNextHit, DelayBetweenHits, false);
+}
+
+void UAT_MultiHitAtLocation::OnDestroy(bool bInOwnerFinished)
+{
+	if (GetWorld())
+	{
+		GetWorld()->GetTimerManager().ClearTimer(TimerHandle);
+	}
+	Super::OnDestroy(bInOwnerFinished);
+}
---
+++ Source/BossRaidGame/GA/AT/AT_MultiHitAtLocation.h
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "Engine/OverlapResult.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "AT_MultiHitAtLocation.generated.h"
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMultiHitHitDelegate, const TArray<FOverlapResult>&, OverlapResults);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FMultiHitFinishedDelegate);
+
+/**
+ * 
+ */
+UCLASS()
+class BOSSRAIDGAME_API UAT_MultiHitAtLocation : public UAbilityTask
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable, Category = "Ability|Tasks", meta = (HidePin = "OwningAbility", DefaultToSelf = "OwningAbility"))
+	static UAT_MultiHitAtLocation* MultiHitAtLocation(
+		UGameplayAbility* OwningAbility,
+		FName TargetLocationKeyName, // FVector 대신 BlackboardKeySelector를 받음
+		int32 NumberOfHits,
+		float DelayBetweenHits,
+		float AreaRadius,
+		TEnumAsByte<ECollisionChannel> TargetChannel,
+		UParticleSystem* AreaVFX, // VFX와 SFX를 태스크가 직접 받도록 추가
+		USoundBase* AreaSFX
+	);
+
+	UPROPERTY(BlueprintAssignable)
+	FMultiHitHitDelegate OnHit;
+
+	UPROPERTY(BlueprintAssignable)
+	FMultiHitFinishedDelegate OnFinished;
+
+protected:
+	virtual void Activate() override;
+	virtual void OnDestroy(bool bInOwnerFinished) override;
+
+private:
+	void PerformNextHit();
+
+	FName TargetLocationKeyName; 
+	int32 NumberOfHits;
+	float DelayBetweenHits;
+	float AreaRadius;
+	TEnumAsByte<ECollisionChannel> TargetChannel;
+	UParticleSystem* AreaVFX;
+	USoundBase* AreaSFX;
+
+	int32 CurrentHitCount;
+	FTimerHandle TimerHandle;
+	TWeakObjectPtr<UBlackboardComponent> BlackboardComp; // 블랙보드 컴포넌트를 캐시
+
+};
