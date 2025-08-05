f3b0c769183d631d137b283dc5757f98710ba061
No1playblock <jghan2000@naver.com>
2025-08-05 15:48:08 +0900

    feat: 구체 추적 기능 개선 및 디버그 추가
    - `SphereTraceForTargets` 메서드의 인자를 구의 중심 위치와 반지름으로 변경하여 시그니처를 간소화함.
    
    - `Activate` 메서드에서 `SphereTraceMulti` 대신 `SphereOverlapActors`를 사용하여 구체의 오버랩을 검사하도록 로직을 변경함.
    
    - 디버그 시각화를 위한 코드 추가로, 구체의 위치와 반지름에 따라 색상을 변경하여 디버그 구체를 그리는 기능을 구현함.
    
    - 타겟 발견 시 브로드캐스트할 수 있는 `OnTargetsFound` 델리게이트를 추가함.
    
    - 불필요한 주석을 제거하여 코드 가독성을 높임.

--- Source/BossRaidGame/GA/AT/AT_SphereTraceForTargets.cpp
+++ Source/BossRaidGame/GA/AT/AT_SphereTraceForTargets.cpp
@@ -5,23 +5,8 @@
 #include "Kismet/KismetSystemLibrary.h"
 #include "GameFramework/Actor.h"
 
-//UAT_SphereTraceForTargets* UAT_SphereTraceForTargets::SphereTraceForTargets(UGameplayAbility* OwningAbility, FVector StartLocation, FVector EndLocation, float Radius, bool bDebug, ECollisionChannel Channel)
-//{
-//	// 새로운 태스크 인스턴스 생성
-//	UAT_SphereTraceForTargets* NewTask = NewAbilityTask<UAT_SphereTraceForTargets>(OwningAbility);
-//
-//	// 팩토리 함수를 통해 받은 파라미터들을 태스크의 멤버 변수에 저장
-//	NewTask->TraceStart = StartLocation;
-//	NewTask->TraceEnd = EndLocation;
-//	NewTask->TraceRadius = Radius;
-//	NewTask->bShowDebug = bDebug;
-//	NewTask->TargetChannel = Channel;
-//	return NewTask;
-//}
-
 UAT_SphereTraceForTargets* UAT_SphereTraceForTargets::SphereTraceForTargets(UGameplayAbility* OwningAbility, FVector TraceSpherePos, float Radius, ECollisionChannel Channel)
 {
-	// 새로운 태스크 인스턴스 생성
 	UAT_SphereTraceForTargets* NewTask = NewAbilityTask<UAT_SphereTraceForTargets>(OwningAbility);
 
 	// 팩토리 함수를 통해 받은 파라미터들을 태스크의 멤버 변수에 저장
@@ -37,20 +22,9 @@ void UAT_SphereTraceForTargets::Activate()
 	ActorsToIgnore.Add(GetAvatarActor()); // 자신의 캐릭터는 검사에서 제외
 
 	TArray<AActor*> HitResults;
-	UE_LOG(LogTemp, Warning, TEXT("SphereTraceForTargets: Channel=%d")
-		, static_cast<int32>(TargetChannel));
-	//UKismetSystemLibrary::SphereTraceMulti(
-	//	this, // World Context Object
-	//	TraceStart,
-	//	TraceEnd,
-	//	TraceRadius,
-	//	UEngineTypes::ConvertToTraceType(TargetChannel), // Pawn 채널과 충돌하는지 확인
-	//	false,
-	//	ActorsToIgnore,
-	//	bShowDebug ? EDrawDebugTrace::ForDuration : EDrawDebugTrace::None,
-	//	HitResults,
-	//	true
-	//);
+	/*UE_LOG(LogTemp, Warning, TEXT("SphereTraceForTargets: Channel=%d")
+		, static_cast<int32>(TargetChannel));*/
+
 	TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypes;
 	ObjectTypes.Add(UEngineTypes::ConvertToObjectType(TargetChannel)); // Pawn 채널과 충돌하는지 확인
 	UKismetSystemLibrary::SphereOverlapActors(
@@ -58,14 +32,23 @@ void UAT_SphereTraceForTargets::Activate()
 		SpherePos,        // 구의 중심 위치
 		SphereRadius,     // 구의 반지름
 		ObjectTypes,      // 검사할 오브젝트 타입들 (Pawn)
-		nullptr,          // 특정 클래스 필터 (사용 안 함)
+		nullptr,          // 특정 클래스 필터
 		ActorsToIgnore,
 		HitResults         // 결과가 저장될 액터 배열
 	);
 
-	// 결과가 있으면 OnTargetsFound 델리게이트를 통해 결과를 브로드캐스트
+	const FColor DebugColor = HitResults.Num() > 0 ? FColor::Green : FColor::Red;
+	DrawDebugSphere(
+		GetWorld(),
+		SpherePos,      // 구의 중심 위치
+		SphereRadius,   // 구의 반지름
+		24,             // 구의 부드러움 (Segment)
+		DebugColor,     // 색상
+		false,          // 계속 유지할지 여부
+		0.2f            // 표시될 시간 (초)
+	);
+
 	OnTargetsFound.Broadcast(HitResults);
 
-	// 태스크의 모든 임무가 끝났으므로 종료
 	EndTask();
 }
--- Source/BossRaidGame/GA/AT/AT_SphereTraceForTargets.h
+++ Source/BossRaidGame/GA/AT/AT_SphereTraceForTargets.h
@@ -22,34 +22,13 @@ public:
 	UPROPERTY(BlueprintAssignable)
 	FTraceResultDelegate OnTargetsFound;
 
-	/**
-	 * 구체 형태의 충돌 검사를 수행하는 태스크를 생성하고 활성화합니다.
-	 * @param OwningAbility 태스크를 소유한 어빌리티
-	 * @param StartLocation 추적 시작 위치
-	 * @param EndLocation 추적 종료 위치
-	 * @param Radius 추적 구체의 반지름
-	 * @param bDebug 디버깅 시각화를 표시할지 여부
-	 */
-	/*UFUNCTION(BlueprintCallable, Category = "Ability|Tasks", meta = (HidePin = "OwningAbility", DefaultToSelf = "OwningAbility"))
-	static UAT_SphereTraceForTargets* SphereTraceForTargets(
-		UGameplayAbility* OwningAbility,
-		FVector StartLocation,
-		FVector EndLocation,
-		float Radius,
-		bool bDebug = false
-		, ECollisionChannel Channel = ECollisionChannel::ECC_Pawn
-	);*/
 	UFUNCTION(BlueprintCallable, Category = "Ability|Tasks", meta = (HidePin = "OwningAbility", DefaultToSelf = "OwningAbility"))
 	static UAT_SphereTraceForTargets* SphereTraceForTargets(UGameplayAbility* OwningAbility, FVector TraceSpherePos, float Radius, ECollisionChannel Channel);
 private:
 	
-	// 태스크가 실제 로직을 수행하는 함수
+
 	virtual void Activate() override;
 
-	/*FVector TraceStart;
-	FVector TraceEnd;
-	float TraceRadius;
-	bool bShowDebug;*/
 
 	FVector SpherePos;
 	float SphereRadius;
