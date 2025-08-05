24425bf8a4df4a1a1a65f3a2ba2842307a7a1aed
No1playblock <jghan2000@naver.com>
2025-08-05 15:36:44 +0900

    feat: 캐릭터 이동 및 상태 관리 기능 추가
    - 이동 상태 효과 클래스를 초기화하는 코드 추가
    - FMobSpawnInfo 데이터를 기반으로 속성 설정 기능 추가
    - 이동 속도 변경 감지를 위한 델리게이트 추가
    - 이동 속도 변경 시 최대 걷기 속도 업데이트 기능 추가
    - 캐릭터 상태 설정 및 효과 적용/제거 기능 추가
    - 클래스 속성과 메서드 선언 추가

---
+++ Source/BossRaidGame/Character/MobGASCharacter.cpp
@@ -0,0 +1,76 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/MobGASCharacter.h"
+#include "Tag/BRGameplayTag.h"
+#include "Attribute/MobCharacterAttributeSet.h"
+#include "GameData/MobSpawnInfo.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+AMobGASCharacter::AMobGASCharacter()
+{
+	static ConstructorHelpers::FClassFinder<UGameplayEffect> MovementEffectFinder(TEXT("/Game/Blueprints/GE/BPGE_SetStateMovement.BPGE_SetStateMovement_C"));
+	if (MovementEffectFinder.Succeeded())
+	{
+		MovementStateEffectClass = MovementEffectFinder.Class;
+	}
+}
+void AMobGASCharacter::InitializeFromData(const FMobSpawnInfo* MobData)
+{
+	if (!MobData || !AttributeSet || !GetCharacterMovement()) return;
+
+	// 체력 설정
+	// AttributeSet의 부모인 UCharacterAttributeSet에 Health, MaxHealth가 있다고 가정
+	ASC->SetNumericAttributeBase(AttributeSet->GetMaxHealthAttribute(), MobData->Health);
+	ASC->SetNumericAttributeBase(AttributeSet->GetHealthAttribute(), MobData->Health);
+
+	// 경험치 보상 설정
+	ASC->SetNumericAttributeBase(AttributeSet->GetExpRewardAttribute(), MobData->Experience);
+
+	ASC->SetNumericAttributeBase(AttributeSet->GetMoveSpeedAttribute(), MobData->WalkSpeed);
+
+	// 이동 속도 설정
+	WalkSpeed = MobData->WalkSpeed;
+	RunSpeed = MobData->RunSpeed;
+	SetWalkingState();
+
+}
+void AMobGASCharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+	if (ASC)
+	{
+		ASC->GetGameplayAttributeValueChangeDelegate(AttributeSet->GetMoveSpeedAttribute()).AddUObject(this, &AMobGASCharacter::OnMovementSpeedChanged);
+	}
+}
+void AMobGASCharacter::OnMovementSpeedChanged(const FOnAttributeChangeData& Data)
+{
+	GetCharacterMovement()->MaxWalkSpeed = Data.NewValue;
+}
+void AMobGASCharacter::SetChasingState()
+{
+	if (!ASC || !MovementStateEffectClass) return;
+
+	if (ASC->HasMatchingGameplayTag(BRTAG_CHARACTER_ISCHASING)) return;
+
+	FGameplayEffectContextHandle EffectContext = ASC->MakeEffectContext();
+	EffectContext.AddSourceObject(this);
+
+	FGameplayEffectSpecHandle SpecHandle = ASC->MakeOutgoingSpec(MovementStateEffectClass, 1.f, EffectContext);
+	if (SpecHandle.IsValid())
+	{
+		// SetByCaller로 뛰는 속도 값을 주입하는 것은 동일
+		SpecHandle.Data->SetSetByCallerMagnitude(BRTAG_DATA_SPEED, RunSpeed);
+
+		ASC->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
+	}
+}
+void AMobGASCharacter::SetWalkingState()
+{
+	if (!ASC) return;
+
+	// "State.Running" 태그를 부여하는 모든 활성화된 이펙트를 찾아서 제거
+	FGameplayTagContainer TagContainer;
+	TagContainer.AddTag(BRTAG_CHARACTER_ISCHASING);
+	ASC->RemoveActiveEffectsWithGrantedTags(TagContainer);
+}
---
+++ Source/BossRaidGame/Character/MobGASCharacter.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/NonPlayerGASCharacter.h"
+#include "MobGASCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOSSRAIDGAME_API AMobGASCharacter : public ANonPlayerGASCharacter
+{
+	GENERATED_BODY()
+	
+public:
+	AMobGASCharacter();
+
+	FORCEINLINE float GetWalkSpeed() const { return WalkSpeed; }
+	FORCEINLINE float GetRunSpeed() const { return RunSpeed; }
+
+	void InitializeFromData(const FMobSpawnInfo* MobData);
+
+
+	void SetChasingState();
+	void SetWalkingState();
+
+protected:
+	
+	virtual void PossessedBy(AController* NewController) override;
+
+
+	virtual void OnMovementSpeedChanged(const FOnAttributeChangeData& Data);
+
+	UPROPERTY(EditAnywhere, Category = "GAS")
+	TSubclassOf<class UGameplayEffect> MovementStateEffectClass;
+
+	float WalkSpeed;
+
+	float RunSpeed;
+};
