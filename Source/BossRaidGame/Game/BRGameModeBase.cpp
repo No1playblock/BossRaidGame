58be2289a1d729bd666d0212f03e37715e9a02e3
No1playblock <jghan2000@naver.com>
2025-08-11 15:20:14 +0900

    feat: 게임 시작 시 남은 시간 관리 기능 추가
    게임 시작 시 RemainingTime을 300으로 설정하고,
    UI에 시간을 업데이트하는 기능을 추가함.
    
    매 초마다 RemainingTime을 감소시키고,
    시간이 0이 되면 타이머를 정리하는 로직으로 임시 구현 (기획대로라면 다음 스테이지로 이동)

--- Source/BossRaidGame/Game/BRGameModeBase.cpp
+++ Source/BossRaidGame/Game/BRGameModeBase.cpp
@@ -2,12 +2,48 @@
 
 
 #include "BRGameModeBase.h"
-
+#include "BRGameState.h"
 ABRGameModeBase::ABRGameModeBase()
 {
 	static ConstructorHelpers::FClassFinder<APawn> CharacterRef(TEXT("/Game/Blueprints/Character/BP_CharacterPlayer.BP_CharacterPlayer_C"));
 	if (CharacterRef.Class)
 	{
 		DefaultPawnClass = CharacterRef.Class;
+	}
+}
+void ABRGameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 게임 시작 시 초기 시간 설정 (예: 300초 = 5분)
+	ABRGameState* GS = GetGameState<ABRGameState>();
+	if (GS)
+	{
+		GS->RemainingTime = 300;
+		// OnRep 함수를 서버에서도 호출하여 호스트 PC의 UI도 업데이트되도록 함
+		GS->BroadcastTimeChanged();
+	}
+
+	// 1초마다 TickTimer 함수를 반복해서 호출하는 타이머 설정
+	FTimerHandle GameTimerHandle;
+	GetWorld()->GetTimerManager().SetTimer(GameTimerHandle, this, &ABRGameModeBase::TickTimer, 1.0f, true);
+}
+
+void ABRGameModeBase::TickTimer()
+{
+	ABRGameState* GS = GetGameState<ABRGameState>();
+	if (GS)
+	{
+		if (GS->RemainingTime > 0)
+		{
+			// 남은 시간을 1초씩 줄임
+			GS->RemainingTime--;
+			// OnRep 함수를 서버에서도 호출
+			GS->BroadcastTimeChanged();
+		}
+		else
+		{
+			GetWorld()->GetTimerManager().ClearAllTimersForObject(this);
+		}
 	}
 }
