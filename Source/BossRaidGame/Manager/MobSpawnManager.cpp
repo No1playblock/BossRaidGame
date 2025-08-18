a3aa2987922855319d75fdfe941d8e58c92dcb6f
No1playblock <jghan2000@naver.com>
2025-08-18 18:11:22 +0900

    feat: 몬스터 풀 관리 및 스폰 로직 개선
    - BeginPlay에서 몬스터 풀 초기화 및 랜덤 몬스터 선택 로직 추가
    - InitializePool 메서드 추가
    - GetPooledMonster 및 ReturnMonsterToPool 메서드 추가
    - SpawnBoss, ChooseRandomMob, RequestSpawnWave 메서드 개선
    - FindSpawnLocation 메서드 수정
    - SpawnSingleMonster 및 SpawnSingleBoss 메서드 개선
    - 코드 가독성 및 유지보수성 향상

--- Source/BossRaidGame/Manager/MobSpawnManager.cpp
+++ Source/BossRaidGame/Manager/MobSpawnManager.cpp
@@ -3,6 +3,7 @@
 
 #include "Manager/MobSpawnManager.h"
 #include "Character/MobGASCharacter.h"
+#include "Character/BossGASCharacter.h"
 #include "NavigationSystem.h"
 #include "NavMesh/NavMeshBoundsVolume.h"
 #include "Kismet/GameplayStatics.h"
@@ -12,176 +13,277 @@
 #include "GameplayEffect.h"
 #include "GameData/MobSpawnInfo.h" 
 #include "GameData/BossSpawnInfo.h" 
-
+#include "Components/CapsuleComponent.h"
 AMobSpawnManager::AMobSpawnManager()
 {
-    PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bCanEverTick = false;
 }
 
 void AMobSpawnManager::BeginPlay()
 {
-    Super::BeginPlay();
+	Super::BeginPlay();
 
-    RequestRandomSpawnWave();
-    FTimerHandle SpawnTimer;
-    GetWorld()->GetTimerManager().SetTimer(SpawnTimer, this, &AMobSpawnManager::RequestRandomSpawnWave, 10.0f, true);
-    
-    if (FindSpawnLocation(BossSpawnLocation))
-    {
-        //해당 위치에 상징 구조물을 먼저 스폰 (설정된 경우)
-        if (BossSymbolicStructureClass)
-        {
-            GetWorld()->SpawnActor<AActor>(BossSymbolicStructureClass, BossSpawnLocation, FRotator::ZeroRotator);
-        }
-    }
+	InitializePool();
 
-    //5분(BossSpawnTime) 후에 보스를 스폰하도록 타이머 설정
-    FTimerHandle BossSpawnTimer;
-    GetWorld()->GetTimerManager().SetTimer(BossSpawnTimer, this, &AMobSpawnManager::SpawnBoss, BossSpawnTime, false);
+	ChooseRandomMob();
+	FTimerHandle RequestTimer;
+	GetWorld()->GetTimerManager().SetTimer(RequestTimer, this, &AMobSpawnManager::ChooseRandomMob, 10.0f, true);
+	GetWorld()->GetTimerManager().SetTimer(SpawnQueueTimerHandle, this, &AMobSpawnManager::ProcessSpawnQueue, 0.1f, true); // 큐 처리 간격을 약간 늘림
+
+	UE_LOG(LogTemp, Warning, TEXT("MobSpawnManager BeginPlay: StartSpawnManager"));
+
+	if (FindSpawnLocation(BossSymbolicStructureClass, BossSpawnLocation))
+	{
+		if (BossSymbolicStructureClass)
+		{
+			GetWorld()->SpawnActor<AActor>(BossSymbolicStructureClass, BossSpawnLocation, FRotator::ZeroRotator);
+		}
+	}
+
+	//5분(BossSpawnTime) 후에 보스를 스폰하도록 타이머 설정
+	FTimerHandle BossSpawnTimer;
+	//GetWorld()->GetTimerManager().SetTimer(BossSpawnTimer, this, &AMobSpawnManager::SpawnBoss, BossSpawnTime, false);
+}
+void AMobSpawnManager::InitializePool()
+{
+	if (!MonsterSpawnTable) return;
+
+	const TArray<FName> RowNames = MonsterSpawnTable->GetRowNames();
+	for (const FName& RowName : RowNames)
+	{
+		FMobSpawnInfo* SpawnInfo = MonsterSpawnTable->FindRow<FMobSpawnInfo>(RowName, TEXT(""));
+		if (SpawnInfo && SpawnInfo->MonsterClass)
+		{
+			// 해당 몬스터 클래스의 풀을 가져오거나 새로 생성
+			FMonsterPool& PoolStruct = InactiveMonsterPool.FindOrAdd(SpawnInfo->MonsterClass);
+
+			for (int32 i = 0; i < InitialPoolSize; ++i)
+			{
+				// 몬스터를 스폰하고 즉시 비활성화하여 풀에 추가
+				ANonPlayerGASCharacter* NewMonster = GetWorld()->SpawnActor<ANonPlayerGASCharacter>(
+					SpawnInfo->MonsterClass,
+					FVector(-10000.f, -10000.f, -10000.f), // 보이지 않는 먼 위치
+					FRotator::ZeroRotator
+				);
+
+				if (NewMonster)
+				{
+					NewMonster->DeactivateCharacter(); // 비활성화
+					PoolStruct.Inactive.Add(NewMonster); // 구조체의 배열에 추가
+				}
+			}
+		}
+	}
 }
 
+ANonPlayerGASCharacter* AMobSpawnManager::GetPooledMonster(TSubclassOf<ACharacter> MonsterClass)
+{
+	// .Inactive 를 통해 배열에 접근
+	TArray<TObjectPtr<ANonPlayerGASCharacter>>& Pool = InactiveMonsterPool.FindOrAdd(MonsterClass).Inactive;
+
+	// 유효한 것 꺼내기 (뒤에서 팝)
+	while (Pool.Num() > 0)
+	{
+		TObjectPtr<ANonPlayerGASCharacter> Obj = Pool.Pop(false);
+		if (IsValid(Obj.Get()))
+		{
+			return Obj.Get();
+		}
+	}
+	return nullptr;
+}
+
+void AMobSpawnManager::ReturnMonsterToPool(ANonPlayerGASCharacter* Monster)
+{
+	if (!IsValid(Monster)) return;
+
+	Monster->DeactivateCharacter();
+
+	// 클래스 키로 풀에 넣기
+	TSubclassOf<ACharacter> Cls = Monster->GetClass();
+	InactiveMonsterPool.FindOrAdd(Cls).Inactive.Add(Monster);
+}
 void AMobSpawnManager::SpawnBoss()
 {
-    if (!BossSpawnTable || BossDataRowName.IsNone()) return;
+	if (!BossSpawnTable || BossDataRowName.IsNone()) return;
 
-    FString ContextString;
-    FBossSpawnInfo* BossInfo = BossSpawnTable->FindRow<FBossSpawnInfo>(BossDataRowName, ContextString);
+	FString ContextString;
+	FBossSpawnInfo* BossInfo = BossSpawnTable->FindRow<FBossSpawnInfo>(BossDataRowName, ContextString);
 
-    if (BossInfo)   // && GameMode
-    {
-		const float DifficultyMultiplier = 1.0f; //GameMode->GetCurrentDifficultyMultiplier();
-        SpawnSingleBoss(BossInfo, BossSpawnLocation, DifficultyMultiplier);
-    }
+	if (BossInfo)
+	{
+		const float DifficultyMultiplier = 1.0f;
+		SpawnSingleBoss(BossInfo, BossSpawnLocation, DifficultyMultiplier);
+	}
 }
 
-void AMobSpawnManager::RequestRandomSpawnWave()
+//랜덤한 몹을 생성
+void AMobSpawnManager::ChooseRandomMob()
 {
-    if (!MonsterSpawnTable) return;
+	if (!MonsterSpawnTable) return;
 
-    const TArray<FName> RowNames = MonsterSpawnTable->GetRowNames();
+	const TArray<FName> RowNames = MonsterSpawnTable->GetRowNames();
 
-    if (RowNames.Num() > 0)
-    {
-        const int32 RandomIndex = FMath::RandRange(0, RowNames.Num() - 1);
-        RequestSpawnWave(RowNames[RandomIndex]);
-    }
+	if (RowNames.Num() > 0)
+	{
+		const int32 RandomIndex = FMath::RandRange(0, RowNames.Num() - 1);
+		RequestSpawnWave(RowNames[RandomIndex]);
+	}
 }
 
+//실제 몹 생성
 void AMobSpawnManager::RequestSpawnWave(const FName& WaveDataRowName)
 {
-    if (!MonsterSpawnTable) return;
+	if (!MonsterSpawnTable) return;
 
-    FString ContextString;
-    FMobSpawnInfo* SpawnInfo = MonsterSpawnTable->FindRow<FMobSpawnInfo>(WaveDataRowName, ContextString);
+	FMobSpawnInfo* SpawnInfo = MonsterSpawnTable->FindRow<FMobSpawnInfo>(WaveDataRowName, TEXT(""));
+	if (!SpawnInfo) return;
 
-    if (SpawnInfo)  // && GameMode
-    {
-        // 웨이브가 생성될 중심 위치
-        FVector WaveCenterLocation;
-        if (FindSpawnLocation(WaveCenterLocation))
-        {
-            // 난이도 배수를 가져옴
-            const float DifficultyMultiplier = 1.0f;//GameMode->GetCurrentDifficultyMultiplier();
-
-            //SpawnCount만큼 몬스터를 중심점 주변에 스폰
-            for (int32 i = 0; i < SpawnInfo->SpawnCount; ++i)
-            {
-                UNavigationSystemV1* NavSys = FNavigationSystem::GetCurrent<UNavigationSystemV1>(GetWorld());
-                if (NavSys)
-                {
-                    FNavLocation SpawnLocation;
-                    // 중심점 주변 반경 내에서 네비게이션 가능한 위치를 찾음
-                    if (NavSys->GetRandomPointInNavigableRadius(WaveCenterLocation, 500.0f, SpawnLocation))     //500.0f 반경
-                    {
-                        // 실제 몬스터 스폰
-                        SpawnSingleMonster(SpawnInfo, SpawnLocation.Location, DifficultyMultiplier);
-                    }
-                }
-            }
-        }
-    }
+	// 멀티스레딩 대신 동기 방식으로 위치 탐색 (풀링이 더 중요한 최적화)
+	FVector WaveCenterLocation;
+	if (FindSpawnLocation(SpawnInfo->MonsterClass, WaveCenterLocation))
+	{
+		for (int32 i = 0; i < SpawnInfo->SpawnCount; ++i)
+		{
+			UNavigationSystemV1* NavSys = UNavigationSystemV1::GetCurrent(GetWorld());
+			if (NavSys)
+			{
+				FNavLocation SpawnLocation;
+				if (NavSys->GetRandomPointInNavigableRadius(WaveCenterLocation, 500.0f, SpawnLocation))
+				{
+					SpawnLocation.Location.Z = WaveCenterLocation.Z;
+					// 스폰 큐에 데이터 추가
+					SpawnQueue.Enqueue(TPair<const FMobSpawnInfo*, FVector>(SpawnInfo, SpawnLocation.Location));
+				}
+			}
+		}
+	}
 }
-
-bool AMobSpawnManager::FindSpawnLocation(FVector& OutLocation)
+void AMobSpawnManager::ProcessSpawnQueue()
 {
-    ANavMeshBoundsVolume* NavVolume = Cast<ANavMeshBoundsVolume>(
-        UGameplayStatics::GetActorOfClass(GetWorld(), ANavMeshBoundsVolume::StaticClass())
-    );
+	if (SpawnQueue.IsEmpty()) return;
 
-    if (!NavVolume) return false;
+	TPair<const FMobSpawnInfo*, FVector> ItemToSpawn;
+	if (SpawnQueue.Dequeue(ItemToSpawn))
+	{
+		SpawnSingleMonster(ItemToSpawn.Key, ItemToSpawn.Value, 1.0f);
+	}
+}
+bool AMobSpawnManager::FindSpawnLocation(TSubclassOf<AActor> ActorClass, FVector& OutLocation)
+{
+	ANavMeshBoundsVolume* NavVolume = Cast<ANavMeshBoundsVolume>(
+		UGameplayStatics::GetActorOfClass(GetWorld(), ANavMeshBoundsVolume::StaticClass())
+	);
+	if (!NavVolume || !ActorClass) return false;
 
-    UNavigationSystemV1* NavSys = FNavigationSystem::GetCurrent<UNavigationSystemV1>(GetWorld());
-    if (NavSys)
-    {
-        const FBox BoundingBox = NavVolume->GetBounds().GetBox();
-        for (int32 i = 0; i < 10; ++i)
-        {
-            const FVector RandomPointInBox = UKismetMathLibrary::RandomPointInBoundingBox(BoundingBox.Min, BoundingBox.Max);
-            FNavLocation NavLocation;
-            if (NavSys->GetRandomPointInNavigableRadius(RandomPointInBox, 500.f, NavLocation))
-            {
-                OutLocation = NavLocation.Location;
-                return true;
-            }
-        }
-    }
-    return false;
+	float CheckHeightOffset = 0.f;
+	FCollisionShape CollisionShape;
+
+	AActor* DefaultActor = ActorClass->GetDefaultObject<AActor>();
+
+	if (ACharacter* DefaultCharacter = Cast<ACharacter>(DefaultActor))
+	{
+		const float CapsuleHalfHeight = DefaultCharacter->GetCapsuleComponent()->GetScaledCapsuleHalfHeight();
+		CheckHeightOffset = CapsuleHalfHeight;
+		CollisionShape = FCollisionShape::MakeCapsule(DefaultCharacter->GetCapsuleComponent()->GetScaledCapsuleRadius(), CapsuleHalfHeight);
+	}
+	else
+	{
+		CheckHeightOffset = 20.f;
+		CollisionShape = FCollisionShape::MakeBox(FVector(50.f, 50.f, 20.f));
+	}
+
+	UNavigationSystemV1* NavSys = UNavigationSystemV1::GetCurrent(GetWorld());
+	if (NavSys)
+	{
+		const FBox BoundingBox = NavVolume->GetBounds().GetBox();
+		for (int32 i = 0; i < 10; ++i)
+		{
+			FNavLocation NavLocation;
+			if (NavSys->GetRandomReachablePointInRadius(GetActorLocation(), BoundingBox.GetExtent().Length(), NavLocation))
+			{
+				FVector CheckLocation = NavLocation.Location;
+				CheckLocation.Z += CheckHeightOffset;
+
+				FCollisionQueryParams QueryParams(SCENE_QUERY_STAT(SpawnCheck), false, this);
+				bool bHasBlockingOverlap = GetWorld()->OverlapBlockingTestByChannel(
+					CheckLocation,
+					FQuat::Identity,
+					ECollisionChannel::ECC_Pawn,
+					CollisionShape,
+					QueryParams
+				);
+
+				//캐릭터들의 피벗이 HalfHeight에 위치하므로,
+				if (!bHasBlockingOverlap)
+				{
+					OutLocation = CheckLocation;
+					return true;
+				}
+			}
+		}
+	}
+	return false;
 }
 
 void AMobSpawnManager::SpawnSingleMonster(const FMobSpawnInfo* SpawnInfo, const FVector& Location, float DifficultyMultiplier)
 {
-    if (!SpawnInfo) return;
+	if (!SpawnInfo) return;
 
-    if (SpawnInfo->SpawnVFX) UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), SpawnInfo->SpawnVFX, Location);
-    if (SpawnInfo->SpawnSFX) UGameplayStatics::PlaySoundAtLocation(GetWorld(), SpawnInfo->SpawnSFX, Location);
+	if (SpawnInfo->SpawnVFX) UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), SpawnInfo->SpawnVFX, Location);
+	if (SpawnInfo->SpawnSFX) UGameplayStatics::PlaySoundAtLocation(GetWorld(), SpawnInfo->SpawnSFX, Location);
 
-
-	FVector SpawnLocation = Location;
-	SpawnLocation.Z += 200.0f; // 약간 위로 스폰 위치 조정 (지면에서 떨어뜨리기)
-    AMobGASCharacter* NewMonster = GetWorld()->SpawnActor<AMobGASCharacter>(SpawnInfo->MonsterClass, Location, FRotator::ZeroRotator);
-
-    if (NewMonster)
-    {
-        NewMonster->InitializeFromData(SpawnInfo);
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+	AMobGASCharacter* NewMonster = Cast<AMobGASCharacter>(GetPooledMonster(SpawnInfo->MonsterClass));
+	if (NewMonster)
+	{
+		// 가져온 몬스터를 활성화하고 위치 설정
+		NewMonster->SetActorLocation(Location);
+		NewMonster->ActivateCharacter();
+		NewMonster->SetOwningSpawnManager(this); // 풀로 돌아올 수 있도록 매니저 정보 전달
+		NewMonster->InitializeFromData(SpawnInfo);
 
 		UE_LOG(LogTemp, Warning, TEXT("Spawned Monster: %s at Location: %s"), *NewMonster->GetName(), *Location.ToString());
-        UAbilitySystemComponent* AbilitySystem = NewMonster->GetAbilitySystemComponent();
-        if (AbilitySystem && StatScalingEffectClass)
-        {
-            FGameplayEffectContextHandle ContextHandle = AbilitySystem->MakeEffectContext();
-            FGameplayEffectSpecHandle SpecHandle = AbilitySystem->MakeOutgoingSpec(StatScalingEffectClass, DifficultyMultiplier, ContextHandle);
-            if (SpecHandle.IsValid())
-            {
-                AbilitySystem->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
-            }
-        }
-    }
+		UAbilitySystemComponent* AbilitySystem = NewMonster->GetAbilitySystemComponent();
+		if (AbilitySystem && StatScalingEffectClass)
+		{
+			FGameplayEffectContextHandle ContextHandle = AbilitySystem->MakeEffectContext();
+			FGameplayEffectSpecHandle SpecHandle = AbilitySystem->MakeOutgoingSpec(StatScalingEffectClass, DifficultyMultiplier, ContextHandle);
+			if (SpecHandle.IsValid())
+			{
+				AbilitySystem->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
+			}
+		}
+	}
 }
 void AMobSpawnManager::SpawnSingleBoss(const FBossSpawnInfo* BossInfo, const FVector& Location, float DifficultyMultiplier)
 {
-    if (!BossInfo) return;
+	if (!BossInfo) return;
 
-    if (BossInfo->SpawnVFX) UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), BossInfo->SpawnVFX, Location);
-    if (BossInfo->SpawnSFX) UGameplayStatics::PlaySoundAtLocation(GetWorld(), BossInfo->SpawnSFX, Location);
+	if (BossInfo->SpawnVFX) UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), BossInfo->SpawnVFX, Location);
+	if (BossInfo->SpawnSFX) UGameplayStatics::PlaySoundAtLocation(GetWorld(), BossInfo->SpawnSFX, Location);
 
-    // 보스 스폰 시에는 FSpawnData가 아닌 FBossSpawnInfo를 사용하므로, InitializeFromData를 직접 호출하기 어려움.
-    // 보스 캐릭터에 별도의 초기화 함수를 만들거나, 스탯을 직접 설정해야 함. 여기서는 스폰만 진행.
-	FVector SpawnLocation = Location;
-	SpawnLocation.Z += 200.0f; // 약간 위로 스폰 위치 조정 (지면에서 떨어뜨리기)
-    ANonPlayerGASCharacter* NewBoss = GetWorld()->SpawnActor<ANonPlayerGASCharacter>(BossInfo->BossClass, SpawnLocation, FRotator::ZeroRotator);
-    if (NewBoss)
-    {
-        // TODO: 보스 캐릭터를 위한 별도의 데이터 초기화 함수 호출 (예: NewBoss->InitializeFromBossData(BossInfo);)
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
 
-        UAbilitySystemComponent* AbilitySystem = NewBoss->GetAbilitySystemComponent();
-        if (AbilitySystem && StatScalingEffectClass)
-        {
-            FGameplayEffectContextHandle ContextHandle = AbilitySystem->MakeEffectContext();
-            FGameplayEffectSpecHandle SpecHandle = AbilitySystem->MakeOutgoingSpec(StatScalingEffectClass, DifficultyMultiplier, ContextHandle);
-            if (SpecHandle.IsValid())
-            {
-                AbilitySystem->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
-            }
-        }
-    }
+
+	BossSpawnLocation.Z += BossInfo->BossClass->GetDefaultObject<ABossGASCharacter>()->GetCapsuleComponent()->GetScaledCapsuleHalfHeight();
+
+	ABossGASCharacter* NewBoss = GetWorld()->SpawnActor<ABossGASCharacter>(BossInfo->BossClass, Location, FRotator::ZeroRotator, SpawnParams);
+
+	if (NewBoss)
+	{
+		NewBoss->InitializeFromData(BossInfo);
+		UAbilitySystemComponent* AbilitySystem = NewBoss->GetAbilitySystemComponent();
+		if (AbilitySystem && StatScalingEffectClass)
+		{
+			FGameplayEffectContextHandle ContextHandle = AbilitySystem->MakeEffectContext();
+			FGameplayEffectSpecHandle SpecHandle = AbilitySystem->MakeOutgoingSpec(StatScalingEffectClass, DifficultyMultiplier, ContextHandle);
+			if (SpecHandle.IsValid())
+			{
+				AbilitySystem->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
+			}
+		}
+	}
 }
