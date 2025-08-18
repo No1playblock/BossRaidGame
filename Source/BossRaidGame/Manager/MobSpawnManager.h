bbffe4172c7e6d78732be66998ae5e2d3ebee7c9
No1playblock <jghan2000@naver.com>
2025-08-18 18:12:03 +0900

    feat: 몬스터 스폰 관리 기능 개선
    - FMobSpawnInfo 및 FBossSpawnInfo 구조체 추가
    - FSpawnQueueItem 및 FMonsterPool 구조체 정의
    - AMobSpawnManager 클래스에 새로운 멤버 함수 추가
    - RequestRandomSpawnWave 함수 제거
    - FindSpawnLocation 함수 시그니처 변경
    - 새로운 UPROPERTY 추가: SpawnDelay, BossSymbolicStructureClass, BossSpawnTime
    - InactiveMonsterPool 및 SpawnQueue를 위한 TMap과 TQueue 추가
    - SpawnQueueTimerHandle을 위한 FTimerHandle 추가
    - 기존 UPROPERTY 및 함수 정리

--- Source/BossRaidGame/Manager/MobSpawnManager.h
+++ Source/BossRaidGame/Manager/MobSpawnManager.h
@@ -9,67 +9,105 @@
 #include "MobSpawnManager.generated.h"
 
 class UGameplayEffect;
+struct FMobSpawnInfo;
+struct FBossSpawnInfo;
+class ANonPlayerGASCharacter;
 
+USTRUCT()
+struct FSpawnQueueItem
+{
+	GENERATED_BODY()
 
+	UPROPERTY()
+	FMobSpawnInfo SpawnInfo;
+
+	UPROPERTY()
+	FVector Location;
+
+	UPROPERTY()
+	float DifficultyMultiplier = 1.0f;
+};
+USTRUCT()
+struct FMonsterPool
+{
+	GENERATED_BODY()
+
+	// 풀에 보관할 몬스터들 (GC에 잡히도록 UPROPERTY + TObjectPtr)
+	UPROPERTY()
+	TArray<TObjectPtr<ANonPlayerGASCharacter>> Inactive;
+};
 UCLASS()
 class BOSSRAIDGAME_API AMobSpawnManager : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
-    AMobSpawnManager();
 
-    // 데이터 테이블의 한 행(Row)을 기반으로 몬스터 웨이브 스폰을 요청하는 함수
-    void RequestSpawnWave(const FName& WaveDataRowName);
+public:
+	AMobSpawnManager();
 
-    // 새로 추가된 함수: 데이터 테이블에서 랜덤한 웨이브를 스폰
-    UFUNCTION(BlueprintCallable, Category = "Spawn")
-    void RequestRandomSpawnWave();
+	void ReturnMonsterToPool(ANonPlayerGASCharacter* Monster);
+
 
 protected:
-    virtual void BeginPlay() override;
+	virtual void BeginPlay() override;
 
 private:
 
-    UFUNCTION() 
-    void SpawnBoss();
+	void InitializePool();
+	ANonPlayerGASCharacter* GetPooledMonster(TSubclassOf<ACharacter> MonsterClass);
 
-    // 스폰 가능한 위치를 찾는 함수
-    bool FindSpawnLocation(FVector& OutLocation);
+	void ChooseRandomMob();
+	void RequestSpawnWave(const FName& WaveDataRowName);
+	void ProcessSpawnQueue();
 
-    // 이펙트/사운드 재생 후, 타이머를 이용해 단일 몬스터를 스폰하는 함수
-    void SpawnSingleMonster(const FMobSpawnInfo* SpawnInfo, const FVector& Location, float DifficultyMultiplier);
+	UFUNCTION()
+	void SpawnBoss();
 
-    void SpawnSingleBoss(const FBossSpawnInfo* BossInfo, const FVector& Location, float DifficultyMultiplier);
+	bool FindSpawnLocation(TSubclassOf<AActor> ActorClass, FVector& OutLocation);
+
+	void SpawnSingleMonster(const FMobSpawnInfo* SpawnInfo, const FVector& Location, float DifficultyMultiplier);
+
+	void SpawnSingleBoss(const FBossSpawnInfo* BossInfo, const FVector& Location, float DifficultyMultiplier);
 
 
 private:
 
-    UPROPERTY(EditDefaultsOnly, Category = "Spawn | General Mob")
-    UDataTable* MonsterSpawnTable;
+	UPROPERTY(EditDefaultsOnly, Category = "Spawn | Pool")
+	int32 InitialPoolSize = 20; 
 
-    UPROPERTY(EditDefaultsOnly, Category = "Spawn | Boss")
-    UDataTable* BossSpawnTable;
+	UPROPERTY(EditDefaultsOnly, Category = "Spawn | General Mob")
+	UDataTable* MonsterSpawnTable;
 
-    UPROPERTY(EditInstanceOnly, Category = "Spawn | Boss")
-    FName BossDataRowName = TEXT("1");
+	UPROPERTY(EditDefaultsOnly, Category = "Spawn | Boss")
+	UDataTable* BossSpawnTable;
 
-    UPROPERTY(EditDefaultsOnly, Category = "Spawn | GAS")
-    TSubclassOf<UGameplayEffect> StatScalingEffectClass;
+	UPROPERTY(EditInstanceOnly, Category = "Spawn | Boss")
+	FName BossDataRowName = TEXT("1");
 
-    // 스폰 딜레이 시간
-    UPROPERTY(EditDefaultsOnly, Category = "Spawn | Config")
-    float SpawnDelay = 0.5f;
+	UPROPERTY(EditDefaultsOnly, Category = "Spawn | GAS")
+	TSubclassOf<UGameplayEffect> StatScalingEffectClass;
 
-    // 보스 스폰 위치 액터
-    UPROPERTY(EditDefaultsOnly, Category = "Spawn | Boss")
-    TSubclassOf<AActor> BossSymbolicStructureClass;
+	// 스폰 딜레이 시간
+	UPROPERTY(EditDefaultsOnly, Category = "Spawn | Config")
+	float SpawnDelay = 0.5f;
+
+	// 보스 스폰 위치 액터
+	UPROPERTY(EditDefaultsOnly, Category = "Spawn | Boss")
+	TSubclassOf<AActor> BossSymbolicStructureClass;
 
 
-    // 보스 스폰 시간
-    UPROPERTY(EditDefaultsOnly, Category = "Spawn | Boss")
-    float BossSpawnTime = 300.0f;
+	// 보스 스폰 시간
+	UPROPERTY(EditDefaultsOnly, Category = "Spawn | Boss")
+	float BossSpawnTime = 300.0f;
 
-    FVector BossSpawnLocation;
+	FVector BossSpawnLocation;
 
+	// 비활성화된 몬스터들을 종류별로 보관하는 맵
+	UPROPERTY()
+	TMap<TSubclassOf<ACharacter>, FMonsterPool> InactiveMonsterPool;
+
+	// 스폰 요청을 저장할 큐 (이제 포인터 대신 실제 데이터를 복사)
+	TQueue<TPair<const FMobSpawnInfo*, FVector>> SpawnQueue;
+
+	// 큐를 처리할 타이머 핸들
+	FTimerHandle SpawnQueueTimerHandle;
 };
