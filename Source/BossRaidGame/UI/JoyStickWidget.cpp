2f9b47981d42bd2caa9fcc42639a50e6ca19db4c
No1playblock <jghan2000@naver.com>
2025-09-07 22:53:57 +0900

    feat: 조이스틱 위젯 생성
    - 조이스틱을 구성하는 뒷배경과 엄지가 움직일 때 따라움직이는 원을 멤버변수로 선언
    - 터치를 시작할 때, 움직일 때, 뗄 때에 맞는 함수 정의 및 선언

---
+++ Source/BossRaidGame/UI/JoyStickWidget.cpp
@@ -0,0 +1,83 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/JoyStickWidget.h"
+#include "Components/Image.h"
+#include "Components/CanvasPanelSlot.h"
+#include "Blueprint/WidgetLayoutLibrary.h"
+
+void UJoyStickWidget::HandleTouchPress(const FVector2D& InTouchLocation)
+{
+	TouchStartLocation = InTouchLocation;
+	CurrentMoveVector = FVector2D::ZeroVector;
+
+	if (JoystickBase && JoystickThumb)
+	{
+		JoystickBase->SetVisibility(ESlateVisibility::Visible);
+		JoystickThumb->SetVisibility(ESlateVisibility::Visible);
+
+		//UE_LOG(LogTemp, Warning, TEXT("Touch Pressed at: %s"), *InTouchLocation.ToString());
+
+		// DPI 보정
+		float ViewportScale = UWidgetLayoutLibrary::GetViewportScale(this);
+		FVector2D LocalPos = InTouchLocation / ViewportScale;
+
+		UE_LOG(LogTemp, Warning, TEXT("Touch:%s  Local:%s  Scale:%f"),
+			*InTouchLocation.ToString(),
+			*LocalPos.ToString(),
+			ViewportScale);
+
+		if (UCanvasPanelSlot* BaseSlot = Cast<UCanvasPanelSlot>(JoystickBase->Slot))
+		{
+			BaseSlot->SetPosition(LocalPos - BaseSlot->GetSize() * 0.5f);
+		}
+
+		if (UCanvasPanelSlot* ThumbSlot = Cast<UCanvasPanelSlot>(JoystickThumb->Slot))
+		{
+			ThumbSlot->SetPosition(LocalPos - ThumbSlot->GetSize() * 0.5f);
+		}
+	}
+}
+
+void UJoyStickWidget::HandleTouchMove(const FVector2D& InTouchLocation)
+{
+	float Scale = UWidgetLayoutLibrary::GetViewportScale(this);
+
+	// DPI 보정된 좌표
+	FVector2D LocalTouchStart = TouchStartLocation / Scale;
+	FVector2D LocalTouchPos = InTouchLocation / Scale;
+
+	UE_LOG(LogTemp, Warning, TEXT("Touch Moved to: %s (Local: %s)"),
+		*InTouchLocation.ToString(), *LocalTouchPos.ToString());
+
+	// 이동량 계산
+	FVector2D Delta = LocalTouchPos - LocalTouchStart;
+
+	const float MaxRadius = 100.f;
+	if (Delta.Size() > MaxRadius)
+	{
+		Delta = Delta.GetSafeNormal() * MaxRadius;
+	}
+
+	CurrentMoveVector = FVector2D(Delta.X, -Delta.Y) / MaxRadius;
+
+
+	if (JoystickThumb)
+	{
+		if (UCanvasPanelSlot* ThumbSlot = Cast<UCanvasPanelSlot>(JoystickThumb->Slot))
+		{
+			ThumbSlot->SetPosition(LocalTouchStart + Delta - ThumbSlot->GetSize() * 0.5f);
+		}
+	}
+}
+
+void UJoyStickWidget::HandleTouchRelease()
+{
+	CurrentMoveVector = FVector2D::ZeroVector;
+
+	if (JoystickBase && JoystickThumb)
+	{
+		JoystickBase->SetVisibility(ESlateVisibility::Hidden);
+		JoystickThumb->SetVisibility(ESlateVisibility::Hidden);
+	}
+}
---
+++ Source/BossRaidGame/UI/JoyStickWidget.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "JoyStickWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOSSRAIDGAME_API UJoyStickWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	void HandleTouchPress(const FVector2D& InTouchLocation);
+	void HandleTouchMove(const FVector2D& InTouchLocation);
+	void HandleTouchRelease();
+
+	// 계산된 이동 벡터를 PlayerController에 전달하기 위한 Getter
+	FVector2D GetMoveVector() const { return CurrentMoveVector; }
+protected:
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<class UImage> JoystickBase;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<class UImage> JoystickThumb;
+
+	/*UPROPERTY(meta = (BindWidget))
+	TObjectPtr<class UCanvasPanel> JoystickCanvas;*/
+
+private:
+	// 조이스틱의 내부 상태를 저장할 변수들
+	FVector2D TouchStartLocation;
+	FVector2D CurrentMoveVector;
+	float JoystickRadius = 100.f; // 조이스틱 배경의 반경
+
+};
