df71413cbd569b3cfe464713baf1094a0170db13
No1playblock <jghan2000@naver.com>
2026-01-25 14:44:52 +0900

    feat: 객체 풀링 인터페이스 추가
    - 객체 생성 및 소멸 시 호출되는 순수 가상 함수 추가.

---
+++ Source/BossRaidGame/Interface/PoolableInterface.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "PoolableInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UPoolableInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class BOSSRAIDGAME_API IPoolableInterface
+{
+	GENERATED_BODY()
+
+
+public:
+/**
+	 * [풀에서 꺼내질 때 호출]
+	 * - 초기화 로직 (속도 리셋, 이펙트 켜기, 체력 복구 등)을 구현하세요.
+	 */
+	UFUNCTION(Category = "Object Pool")
+	virtual void OnPoolSpawned() =0 ;
+
+	/**
+	 * [풀로 돌아갈 때 호출]
+	 * - 정리 로직 (타이머 정지, 이펙트 끄기, 상태 초기화 등)을 구현하세요.
+	 */
+	UFUNCTION(Category = "Object Pool")
+	virtual void OnPoolDespawned() =0 ;
+};
