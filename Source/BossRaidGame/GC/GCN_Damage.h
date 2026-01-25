82582d3e8cbc07f69c2d56e1c846bed19078af54
No1playblock <jghan2000@naver.com>
2026-01-25 14:43:57 +0900

    feat: GCN_Damage 클래스 추가
    - 데미지를 입을때 튀기는 피 VFX를 위한 GCN 구현

---
+++ Source/BossRaidGame/GC/GCN_Damage.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayCueNotify_Static.h"
+#include "GCN_Damage.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOSSRAIDGAME_API UGCN_Damage : public UGameplayCueNotify_Static
+{
+	GENERATED_BODY()
+	
+};
