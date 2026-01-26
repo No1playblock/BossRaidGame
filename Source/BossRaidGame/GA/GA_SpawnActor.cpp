595d71d120abb33647b3ab883bc392083734b994
No1playblock <jghan2000@naver.com>
2026-01-26 14:58:24 +0900

    feat: 스킬 트리 및 쿨타임 시스템 추가
    - ActivateAbility 함수에서 스킬 피해량과 쿨타임을
    가져오는 로직을 추가하고, 쿨타임 효과 및 애니메이션 몽타주 재생 로직을 구현함.
    
    - 스폰 위치와 회전 결정 로직을 개선하고, 객체 풀 시스템을 통해 스폰된 액터를 관리하는 기능을 추가함.

--- Source/BossRaidGame/GA/GA_SpawnActor.cpp
+++ Source/BossRaidGame/GA/GA_SpawnActor.cpp
@@ -2,7 +2,12 @@
 
 
 #include "GA/GA_SpawnActor.h"
-
+#include "Character/GASCharacterPlayer.h"
+#include "Subsystems/ObjectPoolSubsystem.h"
+#include "Components/SkillTreeComponent.h"
+#include "Interface/SkillSpawnedActorInterface.h"
+#include "Abilities/GameplayAbilityTargetTypes.h"
+#include "GameFramework/Actor.h"
 UGA_SpawnActor::UGA_SpawnActor()
 {
 	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
@@ -10,31 +15,120 @@ UGA_SpawnActor::UGA_SpawnActor()
 
 void UGA_SpawnActor::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {
-	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	AActor* AvatarActor = ActorInfo->AvatarActor.Get();
+	AGASCharacterPlayer* PlayerCharacter = Cast<AGASCharacterPlayer>(AvatarActor);
 
-	// 스폰할 액터 클래스가 지정되었는지, 이벤트 데이터가 유효한지 확인
-	if (SpawnActorClass && TriggerEventData)
+	float SkillDamage = 0.0f;
+	float SkillCooldownTime = 0.0f;
+
+	// 스킬 트리 데이터 조회 및 쿨타임/몽타주 처리
+	if (PlayerCharacter)
 	{
-		// 이벤트 데이터로부터 석상 액터 가져옴
-		const AActor* StatueActor = TriggerEventData->Target;
-		if (StatueActor)
+		if (USkillTreeComponent* SkillComp = PlayerCharacter->FindComponentByClass<USkillTreeComponent>())
 		{
-			UWorld* World = GetWorld();
-			if (World)
+			// 현재 어빌리티 클래스로 스킬 데이터 찾기
+			const FSkillTreeDataRow* SkillData = SkillComp->FindSkillDataByGrantedAbility(GetClass());
+			if (SkillData)
 			{
-				// 석상의 위치에 액터를 스폰
-				FVector SpawnLocation = StatueActor->GetActorLocation();
-				SpawnLocation.Z -= 125.0f; // 석상 위에 스폰하도록 Z축 위치를 조정
-				FRotator SpawnRotation = StatueActor->GetActorRotation();
+				SkillDamage = SkillData->SkillDamage;
+				SkillCooldownTime = SkillData->SkillCoolTime;
+			}
+		}
 
+		//쿨타임 수동 적용 
+		if (CooldownEffectClass && SkillCooldownTime > 0.0f)
+		{
+			FGameplayEffectSpecHandle CooldownSpecHandle = MakeOutgoingGameplayEffectSpec(Handle, ActorInfo, ActivationInfo, CooldownEffectClass);
+			if (CooldownSpecHandle.Data.IsValid())
+			{
+				CooldownSpecHandle.Data->SetDuration(SkillCooldownTime, true);
+				if (CooldownTag.IsValid())
+				{
+					CooldownSpecHandle.Data->DynamicGrantedTags.AddTag(CooldownTag);
+				}
+				ApplyGameplayEffectSpecToOwner(Handle, ActorInfo, ActivationInfo, CooldownSpecHandle);
+			}
+		}
+
+		//몽타주 재생
+		if (MontageToPlay)
+		{
+			PlayerCharacter->PlayAnimMontage(MontageToPlay);
+		}
+	}
+
+	// 소환 위치 결정
+	FVector SpawnLocation = FVector::ZeroVector;
+	FRotator SpawnRotation = FRotator::ZeroRotator;
+	AActor* AnchorActor = AvatarActor; // 기본 기준점은 나 자신
+
+	if (TriggerEventData && TriggerEventData->TargetData.Num() > 0)
+	{
+		const FGameplayAbilityTargetData* TargetData = TriggerEventData->TargetData.Get(0);
+		if (TargetData)
+		{
+			if (TargetData->HasHitResult())
+			{
+				SpawnLocation = TargetData->GetHitResult()->Location;
+				// 필요 시 회전값: SpawnRotation = TargetData->GetHitResult()->ImpactNormal.Rotation();
+			}
+			else if (TargetData->HasEndPoint())
+			{
+				SpawnLocation = TargetData->GetEndPoint();
+			}
+		}
+	}
+	
+	//else if (TriggerEventData && TriggerEventData->Target)
+	//{
+	//	AnchorActor = const_cast<AActor*>(TriggerEventData->Target);
+	//}
+
+	if (SpawnLocation.IsZero() && AnchorActor)
+	{
+		SpawnLocation = AnchorActor->GetActorLocation() + (AnchorActor->GetActorQuat().RotateVector(SpawnOffset));
+		SpawnRotation = AnchorActor->GetActorRotation();
+	}
+
+	// 오브젝트 풀을 통한 소환
+	AActor* NewActor = nullptr;
+
+	if (SpawnActorClass)
+	{
+		if (UWorld* World = GetWorld())
+		{
+			// 서브시스템 가져오기
+			if (UObjectPoolSubsystem* PoolSys = World->GetSubsystem<UObjectPoolSubsystem>())
+			{
+				// Owner와 Instigator 설정
+				AActor* OwnerActor = ActorInfo->OwnerActor.Get();
+				APawn* InstigatorPawn = Cast<APawn>(ActorInfo->AvatarActor.Get());
+
+				// 풀에서 소환
+				NewActor = PoolSys->SpawnFromPool(SpawnActorClass, SpawnLocation, SpawnRotation, OwnerActor, InstigatorPawn);
+			}
+			else
+			{
 				FActorSpawnParameters SpawnParams;
-				SpawnParams.Owner = GetOwningActorFromActorInfo();
-				SpawnParams.Instigator = GetOwningActorFromActorInfo()->GetInstigator();
+				SpawnParams.Owner = ActorInfo->OwnerActor.Get();
+				SpawnParams.Instigator = Cast<APawn>(ActorInfo->AvatarActor.Get());
+				SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
 
-				World->SpawnActor<AActor>(SpawnActorClass, SpawnLocation, SpawnRotation, SpawnParams);
+				NewActor = World->SpawnActor<AActor>(SpawnActorClass, SpawnLocation, SpawnRotation, SpawnParams);
 			}
 		}
 	}
 
+	// 소환된 액터 초기화
+	if (NewActor)
+	{
+		// 인터페이스를 통해 데미지와 효과 정보 전달
+		if (NewActor->GetClass()->ImplementsInterface(USkillSpawnedActorInterface::StaticClass()))
+		{
+			ISkillSpawnedActorInterface::Execute_InitializeInfo(NewActor, SkillDamage, DamageEffectClass);
+		}
+	}
+
+	// 어빌리티 종료
 	EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
 }
--- Source/BossRaidGame/GA/GA_SpawnActor.h
+++ Source/BossRaidGame/GA/GA_SpawnActor.h
@@ -21,7 +21,26 @@ protected:
     virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 
 protected:
-    // 블루프린트에서 지정할 힐링 장판 액터 클래스
-    UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "GAS")
-    TSubclassOf<AActor> SpawnActorClass;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Spawning")
+	TSubclassOf<AActor> SpawnActorClass;
+
+	/** 기준 위치로부터의 오프셋 */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Spawning")
+	FVector SpawnOffset;
+
+	/** 데미지 적용을 위한 GE 클래스 */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Damage")
+	TSubclassOf<class UGameplayEffect> DamageEffectClass;
+
+	/** 쿨타임 적용용 GE 클래스 (스킬 트리 시간 적용) */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cooldown")
+	TSubclassOf<class UGameplayEffect> CooldownEffectClass;
+
+	/** 쿨타임 태그  */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cooldown")
+	FGameplayTag CooldownTag;
+
+	/** 스킬 사용 시 재생할 몽타주 */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Animation")
+	TObjectPtr<class UAnimMontage> MontageToPlay;
 };
