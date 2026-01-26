513bc4b2cc5082e660e2443a911d8d7a11818e82
No1playblock <jghan2000@naver.com>
2026-01-26 15:07:16 +0900

    feat: Niagara 모듈 추가
    - `BossRaidGame` 프로젝트에 `"Niagara"` 모듈을 추가하여
    Niagara 기능을 사용할 수 있도록 개선함.

--- Source/BossRaidGame/BossRaidGame.Build.cs
+++ Source/BossRaidGame/BossRaidGame.Build.cs
@@ -8,7 +8,7 @@ public class BossRaidGame : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "AIModule", "GameplayTasks", "NavigationSystem" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "AIModule", "GameplayTasks", "NavigationSystem", "Niagara" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks" });
 
