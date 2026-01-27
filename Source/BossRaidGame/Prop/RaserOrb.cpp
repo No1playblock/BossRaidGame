8d67052837ddd39f77aee9fc4285914021b38471
No1playblock <jghan2000@naver.com>
2026-01-27 15:19:08 +0900

    fix: ARaserOrb의 Tick 호출 비활성화
    -  PrimaryActorTick.bCanEverTick 값을  false로 변경

--- Source/BossRaidGame/Prop/RaserOrb.cpp
+++ Source/BossRaidGame/Prop/RaserOrb.cpp
@@ -17,7 +17,7 @@
 ARaserOrb::ARaserOrb()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 	OrbMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("OrbMesh"));
 	RootComponent = OrbMesh;
